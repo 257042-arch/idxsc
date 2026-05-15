f5dbd85698d7a20a59c98b6902f455093210e636
257042-arch <257042@mansel.id>
2026-05-15 14:38:34 +0700

    Add files via upload

---
+++ IDXScreenerPRO/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "IDX_Screener_PRO"
+include(":app")
